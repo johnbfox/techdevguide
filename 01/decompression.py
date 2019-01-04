9a3fd1b171201e9ff836b23fd8b8e48543e03e5c
John Fox <johnfox@Johns-MacBook-Pro.local>
2019-01-04 07:24:38 -0500

    Added first solution

--- 01/decompression.py
+++ 01/decompression.py
@@ -3,27 +3,19 @@ import re
 expression = r'[0-9]+\[[a-zA-Z]+\]'
 
 def decompress(compressed_data):
-    final_string = compressed_data
-    match = re.match(expression, final_string)
-    while match is not None:
-        new_segment = compute_segment(match.group(0))
+    match = re.search(expression, compressed_data)
+    if match is None:
+        return compressed_data
+    else:
+        new_segment = __compute_segment(match.group(0))
         start, end = match.start(), match.end()
-        final_string = final_string[:start] + new_segment + final_string[end:]
-        print(final_string)
-        match = re.match(expression, final_string)
-        print(match)
-    return final_string
+        return decompress(f'{compressed_data[:start]}{new_segment}{compressed_data[end:]}')
 
-def compute_segment(string):
-    print(string)
-    multiplier = int(re.findall(r'[0-9]+', string)[0])
-    chars = re.findall(r'[a-zA-Z]+', string)[0]
+def __compute_segment(string):
+    multiplier = int(re.search(r'[0-9]+', string).group(0))
+    chars = re.search(r'[a-zA-Z]+', string).group(0)
     return multiplier * chars
 
 if __name__ == '__main__':
     compressed_data = input('Input a string to compress: ')
     print(decompress(compressed_data))
-    #matches = pattern.finditer(compressed_data)
-    #for match in matches:
-    #    print(match)
-    #print(decompress(compressed_data))
